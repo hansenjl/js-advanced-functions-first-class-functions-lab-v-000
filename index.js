8398f5c843006b37a7bfd9c25fa6629344fc99e4
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-09-20 17:43:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,33 +1,8 @@
 // Code your solution in this file!
-const returnFirstTwoDrivers = (drivers) => {
-  var firstTwo = []
-  for(let i = 0; i < 2; i++){
-    firstTwo.push(drivers[i])
-  }
-  return firstTwo
+const returnFirstTwoDrivers = function(drivers){
+  return drivers.slice(0,2) ;
 }
 
-const returnLastTwoDrivers = (drivers) => {
-  var lastTwo = []
-  for(let i = drivers.length - 2; i < drivers.length; i++){
-    lastTwo.push(drivers[i])
-  }
-  return lastTwo
-}
-
-const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
-
-function createFareMultiplier(int){
-  return function(fare){
-    return (fare * int)
-  }
-}
-
-const fareDoubler = createFareMultiplier(2)
-  
-const fareTripler = createFareMultiplier(3)
-
-
-const selectDifferentDrivers = (drivers,fn) => {
-  return fn(drivers)
+const returnLastTwoDrivers = function(drivers){
+  return drivers.slice(-2)
 }
